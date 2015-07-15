db5274efaea45370f01051e253442277d1314000
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-07-15 15:40:13 -0500

    Convert responses to standard Agave format.

--- actors/request_utils.py
+++ actors/request_utils.py
@@ -1,12 +1,16 @@
 import flask.ext.restful.reqparse as reqparse
 from werkzeug.exceptions import ClientDisconnected
 
+TAG = '0.01'
+
 class APIException(Exception):
 
     def __init__(self, message, code=400):
         Exception.__init__(self, message)
         self.message = message
         self.code = code
+        self.status = 'error'
+        self.tag = TAG
 
 class RequestParser(reqparse.RequestParser):
     """Wrap reqparse to raise APIException."""
@@ -17,3 +21,16 @@ class RequestParser(reqparse.RequestParser):
         except ClientDisconnected as exc:
             raise APIException(exc.data['message'], 400)
 
+def ok(result, msg="The request was successful"):
+    d = {'result': result,
+         'status': 'success',
+         'tag': TAG,
+         'msg': msg}
+    return d
+
+def error(result=None, msg="Error processing your request."):
+    d = {'result': result,
+         'status': 'error',
+         'tag': TAG,
+         'msg': msg}
+    return d
