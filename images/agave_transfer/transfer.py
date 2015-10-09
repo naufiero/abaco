d00de702e8b372dc150d2f9b57b960b20d85bc1f
Joe Stubbs <jstubbs@tacc.utexas.edu>
2015-10-09 17:48:37 -0500

    New agave_transfer example actor to demo event-based file system backup.

---
+++ images/agave_transfer/transfer.py
@@ -0,0 +1,39 @@
+# An example abaco actor that reads a
+
+import os
+import requests
+
+# agave client information
+username = os.environ.get('username', 'jdoe')
+password = os.environ.get('password', 'abcde')
+client_key = os.environ.get('client_key', 'iVlh5I4_I6I0b3_NJE2xTUfWAdYa')
+client_secret = os.environ.get('client_secret', 'h4qMtPMGZH7DqsbFD2mA4Ssx9B0a')
+base_url = os.environ.get('base_url', 'https://dev.tenants.staging.agaveapi.co')
+
+
+def get_token():
+    url = '{}/token'.format(base_url)
+    data={'username': username,
+          'password': password,
+          'scope': 'PRODUCTION',
+          'grant_type': 'password'}
+    rsp = requests.post(url, data=data)
+    try:
+        token = rsp.json.get('access_token')
+    except Exception as e:
+        print("Got an exception trying to retrieve an access token: {}".format(e))
+    return token
+
+def transfer_file(token):
+    source_system = ''
+    dest_system = ''
+    print("Transfering file from {} to {}...".format(source_system, dest_system))
+
+def main():
+    msg = os.environ.get('MSG')
+    print('transfer container running for MSG: {}'.format(msg))
+    token = get_token()
+    transfer_file(token)
+
+if __name__ == '__main__':
+    main()
