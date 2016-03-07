61ef5fde40199589b842186ef75124a8cde5af19
Joe Stubbs <joestubbs5@gmail.com>
2016-03-06 20:30:31 -0600

    Shut down workers past ttl.

--- actors/health.py
+++ actors/health.py
@@ -1,28 +1,40 @@
 # Ensures that:
-# 1. all worker containers in the database are still responsive
-# 2. all actors have at least one responsive worker
-# 3. all actors with autoscale=true have a number of workers proportional to the messages in the queue.
+# 1. all worker containers in the database are still responsive; workers that have stopped
+#    responding are shutdown and removed from the database.
+# 2. Enforce ttl for idle workers.
+#
+# In the future, this module will also implement:
+# 3. all actors with stateless=true have a number of workers proportional to the messages in the queue.
 
 # Execute from a container on a schedule as follows:
 # docker run -it --rm -v /var/run/docker.sock:/var/run/docker.sock jstubbs/abaco_core python3 -u /actors/health.py
 
+import time
+
 import channelpy
 
+import codes
+from config import Config
 from docker_utils import rm_container, DockerError
 from models import Actor, delete_worker, get_workers
 from channels import CommandChannel, WorkerChannel
 from stores import actors_store
+from worker import shutdown_worker
 
 def get_actor_ids():
     """Returns the list of actor ids currently registered."""
     return [id for id, _ in actors_store.items()]
 
-def check_workers(actor_id):
+def check_workers(actor_id, ttl):
     """Check health of all workers for an actor."""
     print("Checking health for actors: {}".format(actor_id))
     workers = get_workers(actor_id)
     print("workers: {}".format(workers))
     for worker in workers:
+        # ignore workers on different hosts
+        if not Config.get('spawner', 'host_id') == worker['host_id']:
+            continue
+        # first check if worker is responsive; if not, will need to manually kill
         print("Checking health for worker: {}".format(worker))
         ch = WorkerChannel(name=worker['ch_name'])
         try:
@@ -42,30 +54,42 @@ def check_workers(actor_id):
             delete_worker(actor_id, worker['ch_name'])
         else:
             print("Worker ok.")
+        # now check if the worker has been idle beyond the ttl:
+        if ttl < 0:
+            # ttl < 0 means infinite life
+            print("Infinite ttl configured; leaving worker")
+            return
+        if worker['status'] == codes.READY and \
+            worker['last_execution'] + ttl < time.time():
+            # shutdown worker
+            print("Shutting down worker beyond ttl.")
+            shutdown_worker(worker['ch_name'])
+        else:
+            print("Worker still has life.")
 
-def add_workers(actor_id):
-    """Add workers for an actor if necessary."""
-    print("Entering add_workers for {}".format(actor_id))
+def manage_workers(actor_id):
+    """Scale workers for an actor if based on message queue size and policy."""
+    print("Entering manage_workers for {}".format(actor_id))
     try:
         actor = Actor.from_db(actors_store[actor_id])
     except KeyError:
         print("Did not find actor; returning.")
         return
     workers = get_workers(actor_id)
-    if len(workers) < 1:
-        print("Found zero workers, starting another worker.")
-        ch = CommandChannel()
-        ch.put_cmd(actor_id=actor.id, image=actor.image, num=1, stop_existing=False)
-    else:
-        print("Found at least one worker.")
+    #TODO - implement policy
 
 def main():
-    print("Running abaco health checks...")
+    print("Running abaco health checks. Now: {}".format(time.time()))
+    ttl = Config.get('workers', 'worker_ttl')
+    try:
+        ttl = int(ttl)
+    except Exception:
+        ttl = -1
     ids = get_actor_ids()
     print("Found {} actor(s). Now checking status.".format(len(ids)))
     for id in ids:
-        check_workers(id)
-        add_workers(id)
+        check_workers(id, ttl)
+        # manage_workers(id)
 
 
 if __name__ == '__main__':
