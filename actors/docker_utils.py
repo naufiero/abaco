82f5efe9ff9502d9c7884cce4d496a6ae3703459
Joe Stubbs <joestubbs5@gmail.com>
2016-03-06 20:26:38 -0600

    Fix stats collection timeouts and add support for host_id and host_ip.

--- actors/docker_utils.py
+++ actors/docker_utils.py
@@ -1,5 +1,6 @@
 import json
 import os
+import time
 import timeit
 
 import docker
@@ -15,6 +16,9 @@ AE_IMAGE = os.environ.get('AE_IMAGE', 'jstubbs/abaco_core')
 max_run_time = int(Config.get('workers', 'max_run_time'))
 
 dd = Config.get('docker', 'dd')
+host_id = Config.get('spawner', 'host_id')
+host_ip = Config.get('spawner', 'host_ip')
+
 
 class DockerError(Exception):
     def __init__(self, message):
@@ -67,10 +71,13 @@ def run_worker(image, ch_name):
     cli.start(container=container.get('Id'), binds=binds)
     return { 'image': image,
              # @todo - location will need to change to support swarm or multi-node compute cluster.
-             'location': 'unix://var/run/docker.sock',
+             'location': dd,
              'cid': container.get('Id'),
              'ch_name': ch_name,
-             'status': BUSY}
+             'status': BUSY,
+             'host_id': host_id,
+             'host_ip': host_ip,
+             'last_execution': time.time()}
 
 def execute_actor(actor_id, worker_ch, image, msg, d={}, privileged=False):
     result = {'cpu': 0,
@@ -100,8 +107,10 @@ def execute_actor(actor_id, worker_ch, image, msg, d={}, privileged=False):
     start = timeit.default_timer()
     update_worker_status(actor_id, worker_ch, BUSY)
     running = True
+    # create a separate cli for checkin stats objects since these should be fast and we don't want to wait
+    stats_cli = docker.AutoVersionClient(base_url=dd, timeout=1)
     try:
-        stats_obj = cli.stats(container=container.get('Id'), decode=True)
+        stats_obj = stats_cli.stats(container=container.get('Id'), decode=True)
     except ReadTimeout:
         # if the container execution is so fast that the inital stats object cannot be created,
         # we skip the running loop and return a minimal stats object
@@ -110,31 +119,40 @@ def execute_actor(actor_id, worker_ch, image, msg, d={}, privileged=False):
         return result
     while running:
         try:
+            print("waiting on a stats obj: {}".format(timeit.default_timer()))
             stats = next(stats_obj)
         except ReadTimeoutError:
+            print("next(stats) just timed out: {}".format(timeit.default_timer()))
             # container stopped before another stats record could be read, just ignore and move on
             running = False
+            break
         try:
             result['cpu'] += stats['cpu_stats']['cpu_usage']['total_usage']
             result['io'] += stats['network']['rx_bytes']
         except KeyError:
             # as of docker 1.9, the stats object returns bytes that must be decoded
             # and the network key is now 'networks' with multiple subkeys.
+            print("got a stats obj: {}".format(timeit.default_timer()))
             if type(stats) == bytes:
                 stats = json.loads(stats.decode("utf-8"))
             result['cpu'] += stats['cpu_stats']['cpu_usage']['total_usage']
             result['io'] += stats['networks']['eth0']['rx_bytes']
-
+            print("Recorded a stats obj:".format(timeit.default_timer()))
         if running:
             try:
+                print("waiting on cli.wait: {}".format(timeit.default_timer()))
                 cli.wait(container=container.get('Id'), timeout=1)
+                print("container finished: {}".format(timeit.default_timer()))
                 running = False
             except ReadTimeout:
+                print("cli.wait just timed out: {}".format(timeit.default_timer()))
                 # the wait timed out so check if we are beyond the max_run_time
                 runtime = timeit.default_timer() - start
                 if max_run_time > 0 and max_run_time < runtime:
+                    print("hit runtime limit: {}".format(timeit.default_timer()))
                     cli.stop(container.get('Id'))
                     running = False
+    print("container stopped:{}".format(timeit.default_timer()))
     stop = timeit.default_timer()
     # get logs from container
     logs = cli.logs(container.get('Id'))
