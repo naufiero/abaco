4458386b0fde89b580dc5d9ad313d6a92f04ae00
Joe Stubbs <joestubbs5@gmail.com>
2016-03-14 14:36:40 -0500

    Add support for gunicorn.

--- entry.sh
+++ entry.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-
-python3 -u /actors/reg_api.py
+if [ $api = "reg" ]; then
+    if [ $server = "dev" ]; then
+        python3 -u /actors/reg_api.py
+    else
+        /usr/local/bin/gunicorn -w 2 -b :5000 reg_api:app
+    fi
+elif [ $api = "admin" ]; then
+    if [ $server = "dev" ]; then
+        python3 -u /actors/admin_api.py
+    else
+        /usr/local/bin/gunicorn -w 2 -b :5000 admin_api:app
+    fi
+elif [ $api = "mes" ]; then
+    if [ $server = "dev" ]; then
+        python3 -u /actors/mes_api.py
+    else
+        /usr/local/bin/gunicorn -w 2 -b :5000 mes_api:app
+    fi
+fi
 
 sleep infinity
