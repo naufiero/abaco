dc5a37dad1b8900d0ff6903e9dfbcb17a2af72ff
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-03-17 15:20:24 -0500

    Reference the correct gunicorn binary and flask modules.

--- entry.sh
+++ entry.sh
@@ -4,20 +4,20 @@ if [ $api = "reg" ]; then
     if [ $server = "dev" ]; then
         python3 -u /actors/reg_api.py
     else
-        /usr/local/bin/gunicorn -w 2 -b :5000 reg_api:app
+        cd /actors; /usr/bin/gunicorn -w 2 -b :5000 reg_api:app
     fi
 elif [ $api = "admin" ]; then
     if [ $server = "dev" ]; then
         python3 -u /actors/admin_api.py
     else
-        /usr/local/bin/gunicorn -w 2 -b :5000 admin_api:app
+        cd /actors; /usr/bin/gunicorn -w 2 -b :5000 admin_api:app
     fi
 elif [ $api = "mes" ]; then
     if [ $server = "dev" ]; then
         python3 -u /actors/mes_api.py
     else
-        /usr/local/bin/gunicorn -w 2 -b :5000 mes_api:app
+        cd /actors; /usr/bin/gunicorn -w 2 -b :5000 message_api:app
     fi
 fi
 
-sleep infinity
+while true; do sleep 86400; done
