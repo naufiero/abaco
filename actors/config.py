06f5a9517da0718cb6a96aa3918e5f6090d37f94
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-03-17 15:16:42 -0500

    Read exactly one config file.

--- actors/config.py
+++ actors/config.py
@@ -1,6 +1,7 @@
 """Read config files from:
-- Deployed abaco.conf inside package
-- /etc/abaco.conf
+- /abaco.conf (added by user)
+- /etc/abaco.conf (an example is placed here by the Dockerfile)
+
 """
 
 from configparser import ConfigParser
@@ -11,11 +12,16 @@ HERE = os.path.dirname(os.path.abspath(__file__))
 
 def read_config():
     parser = ConfigParser()
-    places = [os.path.abspath(os.path.join(HERE, '../abaco.conf')),
-              os.path.expanduser('/etc/abaco.conf')]
-    if not parser.read(places):
+    places = ['/abaco.conf',
+              '/etc/abaco.conf']
+    place = places[0]
+    for p in places:
+        if os.path.exists(p):
+            place = p
+            break
+    if not parser.read(place):
         raise RuntimeError("couldn't read config file from {0}"
-                           .format(', '.join(places)))
+                           .format(', '.join(place)))
     return parser
 
 Config = read_config()
