001debe614caa8d8c87e2d6e1b96ab6ccd333a91
Joe Stubbs <jstubbs@tacc.utexas.edu>
2016-08-07 22:35:10 -0500

    (18): mongo storage driver.

--- actors/store.py
+++ actors/store.py
@@ -3,7 +3,7 @@ import collections
 import json
 
 import redis
-
+from pymongo import MongoClient
 
 def _do_get(getter, key):
     obj = getter(key)
@@ -19,7 +19,6 @@ def _do_get(getter, key):
 def _do_set(setter, key, value):
     obj = json.dumps(value)
     setter(key, obj.encode('utf-8'))
-
 
 class StoreMutexException(Exception):
     pass
@@ -142,3 +141,87 @@ class Store(AbstractStore):
         value = self._db.getset(key, json.dumps(value).encode('utf-8'))
         if value is not None:
             return json.loads(value.decode('utf-8'))
+
+
+class MongoStore(AbstractStore):
+
+    def __init__(self, host, port, database='abaco', db='0'):
+        """
+        Creates an abaco `store` which maps to a single mongo
+        collection within some database.
+        :param host: the IP address of the mongo server.
+        :param port: port of the mongo server.
+        :param database: the mongo database to use for abaco.
+        :param db: an integer mapping to a mongo collection within the
+        mongo database.
+
+        :return:
+        """
+        mongo_uri = 'mongodb://{}:{}'.format(host, port)
+        self._mongo_client = MongoClient(mongo_uri)
+        self._mongo_database = self._mongo_client[database]
+        self._db = self._mongo_database[db]
+
+    def __getitem__(self, key):
+        result = self._db.find_one({'_id': key})
+        if not result:
+            raise KeyError()
+        # return self._decode(result[key])
+        return result[key]
+
+    def __setitem__(self, key, value):
+        # self._db.save({'_id': key, key: self._encode(value)})
+        self._db.save({'_id': key, key: value})
+
+    def __delitem__(self, key):
+        self._db.delete_one({'_id': key})
+
+    def __iter__(self):
+        for cursor in self._db.find():
+            yield cursor['_id']
+        # return self._db.scan_iter()
+
+    def __len__(self):
+        return self._db.count()
+
+    def _prepset(self, value):
+        if type(value) is bytes:
+            return value.decode('utf-8')
+        return value
+
+    def set_with_expiry(self, key, obj, ex):
+        """Set `key` to `obj` with automatic expiration of `ex` seconds."""
+        self._db.save({'_id': key, 'exp': ex, key: self._prepset(obj)})
+
+    def update(self, key, field, value):
+        "Atomic ``self[key][field] = value``."""
+        # result = self._db.find_and_modify(query={'_id': key},
+        #                          update={'$set': {'{}.{}'.format(key,field): self._encode(value)}})
+        result = self._db.find_and_modify(query={'_id': key},
+                                 update={'$set': {'{}.{}'.format(key,field): value}})
+        if not result:
+            raise KeyError()
+
+    def pop_field(self, key, field):
+        "Atomic pop ``self[key][field]``."""
+        # result = self._db.find_and_modify(query={'_id': key},
+        #                                   update={'$unset': {'{}.{}'.format(key, self._encode(field)): ''}})
+        result = self._db.find_and_modify(query={'_id': key},
+                                          update={'$unset': {'{}.{}'.format(key, field): ''}})
+        result = result.get(key)
+        # return self._decode(result[key])
+        return result[key]
+
+    def update_subfield(self, key, field1, field2, value):
+        "Atomic ``self[key][field1][field2] = value``."""
+        # self._db.update_one({'_id': key}, {'$set': {'{}.{}.{}'.format(key, field1, field2): self._encode(value)}})
+        self._db.update_one({'_id': key}, {'$set': {'{}.{}.{}'.format(key, field1, field2): value}})
+
+    def getset(self, key, value):
+        "Atomically: ``self[key] = value`` and return previous ``self[key]``."
+        # value = self._db.find_and_modify(query={'_id': key},
+        #                                  update={key: self._encode(value)})
+        value = self._db.find_and_modify(query={'_id': key},
+                                         update={key: value})
+        # return self._decode(value[key])
+        return value[key]
--- actors/stores.py
+++ actors/stores.py
@@ -1,14 +1,23 @@
 from functools import partial
 
-from store import Store
+from store import Store, MongoStore
 from config import Config
 
+if Config.get('store', 'use_mongo'):
+    config_store = partial(
+        MongoStore, Config.get('store', 'host'), Config.getint('store', 'port'))
 
-config_store = partial(
-    Store, Config.get('store', 'host'), Config.getint('store', 'port'))
+    actors_store = config_store(db='1')
+    workers_store = config_store(db='2')
+    logs_store = config_store(db='3')
+    permissions_store = config_store(db='4')
+    executions_store = config_store(db='5')
+else:
+    config_store = partial(
+        Store, Config.get('store', 'host'), Config.getint('store', 'port'))
 
-actors_store = config_store(db=1)
-workers_store = config_store(db=2)
-logs_store = config_store(db=3)
-permissions_store = config_store(db=4)
-executions_store = config_store(db=5)
+    actors_store = config_store(db=1)
+    workers_store = config_store(db=2)
+    logs_store = config_store(db=3)
+    permissions_store = config_store(db=4)
+    executions_store = config_store(db=5)
