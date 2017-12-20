d1b1c4ef0ba5691d5b250d80683b4493a91543fa
Joe Stubbs <jstubbs@tacc.utexas.edu>
2017-12-19 18:44:07 -0600

    minimal test skeleton

--- tests/rabbit_abaco_tests.py
+++ tests/rabbit_abaco_tests.py
@@ -19,14 +19,88 @@ import requests
 
 from util import base_url, case, headers, basic_response_checks
 
+from actors import channels, codes, stores, models
+
 
 # whether to first delete the workers before deleting the actor.
 FIRST_DELETE_WORKERS = os.environ.get('FIRST_DELETE_WORKERS', "True")
 
 print("rabbit_abaco_tests running with: {}. {}. {}".format(base_url, case, FIRST_DELETE_WORKERS))
 
+def create_actor_object():
+    args = {'image': 'abacosamples/test',
+            'tenant': 'TACC-PROD',
+            'owner': 'jstubbs',
+            'api_server': 'https://api.tacc.utexas.edu',
+            'mounts': []
+    }
+    actor = models.Actor(**args)
+    stores.actors_store[actor.db_id] = actor.to_db()
+    print("Stored new actor in actors_store. actor_id: {}".format(actor['id']))
+    return actor
+
+def create_execution_object(actor):
+    return models.Execution.add_execution(actor.dbid, {'cpu': 0,
+                                                       'io': 0,
+                                                       'runtime': 0,
+                                                       'status': codes.SUBMITTED,
+                                                       'executor': 'jstubbs'})
+
+
+def minimal_test():
+    """
+    This test uses the channels
+    :return:
+    """
+    # create an actor object in the actors_store
+    actor = create_actor_object()
+    aid = actor.db_id
+
+    # send spawner a message to start a worker for a new actor
+    worker_id = models.Worker.ensure_one_worker(aid, actor.tenant)
+    worker_ids = [worker_id]
+    ch = channels.CommandChannel()
+    ch.put_cmd(actor_id=aid,
+               worker_ids=worker_ids,
+               image=actor.image,
+               tenant=actor.tenant,
+               num=1,
+               stop_existing=False)
+
+    # send a message to the actor's inbox
+    eid = create_execution_object(actor)
+    ch = channels.ActorMsgChannel(actor=aid)
+    d = {}
+    d['_abaco_username'] = 'jstubbs'
+    d['_abaco_api_server'] = actor.api_server
+    d['_abaco_execution_id'] = eid
+    d['_abaco_Content_Type'] = 'str'
+    ch.put_msg(message='test', d=d)
+    ch.close()
+
+    # wait for execution to complete:
+    wait_for_execution(aid, eid)
+
+
+def wait_for_execution(aid, eid):
+    # wait for execution to complete:
+    count = 0
+    while count < 10:
+        time.sleep(1)
+        url = '{}/actors/{}/executions/{}'.format(base_url, aid, eid)
+        rsp = requests.get(url, headers=headers())
+        result = basic_response_checks(rsp)
+        status = result.get('status')
+        if status == 'COMPLETE':
+            print("execution complete")
+            break
+    else:
+        print("execution didn't complete. exiting")
+        sys.exit()
+
 
 def basic_test():
+    """This test uses the http API to create, execute and delete an actor."""
     # create an actor
     url = '{}/actors'.format(base_url)
     data = {'image': 'abacosamples/test'}
@@ -47,19 +121,7 @@ def basic_test():
     print("got execution: {}".format(eid))
 
     # wait for execution to complete:
-    count = 0
-    while count < 10:
-        time.sleep(1)
-        url = '{}/actors/{}/executions/{}'.format(base_url, aid, eid)
-        rsp = requests.get(url, headers=headers())
-        result = basic_response_checks(rsp)
-        status = result.get('status')
-        if status == 'COMPLETE':
-            print("execution complete")
-            break
-    else:
-        print("execution didn't complete. exiting")
-        sys.exit()
+    wait_for_execution(aid, eid)
 
     # delete workers first, if configured:
     if FIRST_DELETE_WORKERS == 'True':
