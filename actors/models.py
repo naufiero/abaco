fffe234ed426e08bdd7989205e67fc96b38ca6f7
Joe Stubbs <jstubbs@tacc.utexas.edu>
2018-04-26 18:07:50 -0500

    fix issue where docker fails to report container execution finish time under heavy load; in these case we now return a finish_time computed from the start_time and run_time.

--- actors/models.py
+++ actors/models.py
@@ -681,8 +681,7 @@ class ExecutionsSummary(AbacoDAO):
         ('api_server', 'derived', 'api_server', str, 'Base URL for the tenant that associated actor belongs to.', None),
         ('actor_id', 'derived', 'actor_id', str, 'id for the actor.', None),
         ('owner', 'provided', 'owner', str, 'The user who created the associated actor.', None),
-        ('executions', 'dervied', 'executions', list, 'List of all execution ids.', None),
-        # ('ids', 'derived', 'ids', list, 'List of all execution ids.', None),
+        ('executions', 'derived', 'executions', list, 'List of all executions with summary fields.', None),
         ('total_executions', 'derived', 'total_executions', str, 'Total number of execution.', None),
         ('total_io', 'derived', 'total_io', str,
          'Block I/O usage, in number of 512-byte sectors read from and written to, by all executions.', None),
@@ -715,6 +714,12 @@ class ExecutionsSummary(AbacoDAO):
                          'message_received_time': val.get('message_received_time')}
             if val.get('final_state'):
                 execution['finish_time'] = val.get('final_state').get('FinishedAt')
+                # we rely completely on docker for the final_state object which includes the FinishedAt time stamp;
+                # under heavy load, we have seen docker fail to set this time correctly and instead set it to 1/1/0001.
+                # in that case, we should use the total_runtime to back into it.
+                if execution['finish_time'].startswith('0001-01-01'):
+                    finish_time = float(val.get('start_time')) + float(val['runtime'])
+                    execution['finish_time'] = display_time(str(finish_time))
             tot['executions'].append(execution)
             tot['total_executions'] += 1
             tot['total_cpu'] += int(val['cpu'])
