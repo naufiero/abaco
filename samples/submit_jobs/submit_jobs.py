b112e80a43ae2ab30d4ae52c8cbd7ef3f4579a4f
Joe Stubbs <jstubbs@tacc.utexas.edu>
2018-07-31 05:36:40 -0500

    Fix client creation and message retrieval

--- samples/submit_jobs/submit_jobs.py
+++ samples/submit_jobs/submit_jobs.py
@@ -3,8 +3,9 @@ from agavepy.actors import get_context, get_client
 def main():
 
     context = get_context()
+    ag = get_client()
     try:
-        job = ag.jobs.submit(body=context['msssage_dict'])
+        job = ag.jobs.submit(body=context['message_dict'])
         print("jobs {} submitted successfully.".format(job))
     except Exception as e:
         print("Exception submitting job: {}".format(e))
