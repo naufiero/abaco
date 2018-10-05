14bed018e3f30b47d444690e1b6559fcf46b91c7
bkuritz <brandikuritz@gmail.com>
2018-10-05 09:09:57 -0500

    Merge branch 'dev' of https://github.com/TACC/abaco into kwhitley54-dashboard
    # Conflicts:
    #	actors/spawner.py
    #	docker-compose-dashboard.yml

--- dashboard/abaco/tests.py
+++ dashboard/abaco/tests.py
@@ -1,12 +1,39 @@
 from django.test import TestCase
 
-#
-# 1. flushes session if not admin
-# 2. does not allow login if not admin
+rom rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
+
+from dashboard.local_secrets import *
 
 
 
-def test_no_login_if_not_admin(self):
-    pass
+class DashboardClassCase(TestCase):
+    def setup(self):
+
+        token_tsu = Token.objects.get(user__username='testshareuser')
+
+        self.client = APIClient()
+
+    def test_no_login_if_not_admin(self):
+        token_tu = Token.objects.get(user__username='testuser')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token_tu.key)
+        print
+
+
+
+    def test_flushes_session_if_not_admin(self):
+        # self.client.login(username='testuser', password='{PW_TU}')
+        pass
+
+    def test_actors_tab_no_session(self):
+        pass
+
+    def test_workers_tab_no_session(self):
+        pass
+
+    def test_executions_tab_no_session(self):
+        pass
+
+
 
 
