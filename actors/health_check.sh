4e1a7b9253047a77609e1a2837bac1df68a613fc
Joe Stubbs <jstubbs@tacc.utexas.edu>
2019-05-28 16:34:39 -0500

    better comments

--- actors/health_check.sh
+++ actors/health_check.sh
@@ -5,7 +5,7 @@
 # give initial processes some time to launch
 sleep 30 #120
 
-# main loop runs every 10 minutes
+# main loop runs every 10 minutes/20 seconds
 while :
 do
     python3 -u /actors/health.py
